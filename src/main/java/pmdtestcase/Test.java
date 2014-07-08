cf9b0018e4626c05aad6dc9ce516d2f097d1167c
Rob Worsnop <robworsnop267@air-watch.com>
2014-07-08 16:17:43 -0400

    Add files

---
+++ src/main/java/pmdtestcase/Test.java
@@ -0,0 +1,13 @@
+package pmdtestcase;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by RWorsnop on 7/8/2014.
+ */
+public class Test {
+    public static void main(String[] args) {
+        new String("s");
+        Consumer<String> c = s -> {};
+    }
+}
